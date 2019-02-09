c28ab6282dbfc3b4dd69a18b2f630798b868c3d6
Divya Vashistha <divyavashishtha23@gmail.com>
2019-02-09 16:45:14 +0530

    dot net angular commit

---
+++ DOT NET API/MovieAPIDemo/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieAPIDemo.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string DateReleased { get; set; }
+        public int Rating { get; set; }
+        public Genre Genre { get; set; }
+        [ForeignKey("Genre")]
+        public int Genre_Id { get; set; }
+    }
+}
